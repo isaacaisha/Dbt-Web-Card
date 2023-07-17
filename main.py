772971fc46ded48a04516b9fbc83f9d137b1a548
isaacaisha <medusadbt@gmail.com>
2023-07-18 00:35:40 +0200

    Update Flask main configuration

--- main.py
+++ main.py
@@ -22,6 +22,18 @@ def home():
     # return 'Siisi Chacal, Once Again👌🏿🙃💪🏿'
 
 
+@app.route('/index.html', methods=['POST'])
+def home_():
+    time_sec = time.localtime()
+    current_year = time_sec.tm_year
+    # getting the current date and time
+    current_datetime = datetime.now()
+    # getting the time from the current date and time in the given format
+    current_time = current_datetime.strftime("%a/%d/%B")
+    return render_template('index.html', date=current_time, year=current_year)
+    # return 'Siisi Chacal, Once Again👌🏿🙃💪🏿'
+
+
 @app.route('/style/<name>')
 def style(name=None):
     return render_template('style.css', name=name)
