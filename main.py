35a742585a9bef52b6a47cffe0c4d0d0bc6733df
isaacaisha <medusadbt@gmail.com>
2023-07-18 01:16:28 +0200

    Update Flask app configuration

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from datetime import datetime
 import time
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/favicon.ico')
