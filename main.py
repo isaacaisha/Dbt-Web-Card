d7f8c7b911ecdaaa7f411971fc02b1f94648b16c
isaacaisha <medusadbt@gmail.com>
2023-07-17 02:46:05 +0200

    requirements.txt init

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
-
-import main
+import random
+import time
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/home/<name>')
+@app.route('/')
 def home():
-    # Your code here
-    return render_template('index.html')
+    time_sec = time.localtime()
+    current_year = time_sec.tm_year
+    random_number = random.randint(1, 9)
+    return render_template('index.html', num=random_number, year=current_year)
+    # return 'Siisi Chacal, Once Again👌🏿🙃💪🏿'
 
 
 @app.route('/style/<name>')
 def style(name=None):
-    return app.send_static_file(name)
+    return render_template('style.css', name=name)
 
 
 if __name__ == '__main__':
